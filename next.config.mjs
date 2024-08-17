78bfb5f359297cf97af5189534b0d21cbbe105cd
unknown <105841111022@student.unismuh.ac.id>
2024-08-17 14:09:26 +0800

    selesai capter 10

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        ppr: 'incremental',
+      },
+    };
+     
+    export default nextConfig;
 
-export default nextConfig;
+
+
